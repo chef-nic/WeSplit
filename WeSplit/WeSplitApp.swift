3a9bcacb9d33179e70bb79ec99e56a0c09129e89
chef-nic <nicholasajohnson@me.com>
2024-07-09 10:13:39 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nicholas Johnson on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
